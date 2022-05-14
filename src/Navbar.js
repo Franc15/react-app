0048937cbb6f7b297518d144b8af6f0489a06e5f
Franc <francissteven1@gmail.com>
2022-05-14 10:00:06 +0300

    auth0 integration

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,19 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+
 function Navbar() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <nav className="md:flex justify-between m-2">
       <h2 className="text-2xl text-green-600 p-2">Codeninjas</h2>
       <ul className="md:flex">
         <li className="p-2">Home</li>
         <li className="p-2">About</li>
-        <button className="bg-red-300 rounded-sm text-white p-2 hover:bg-red-400 dark:bg-green-400">
-          Contact
-        </button>
       </ul>
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
     </nav>
   );
 }
---
+++ src/auth0-provider-with-history.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const history = useNavigate();
+
+  const onRedirectCallback = (appState) => {
+    history.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  };
+
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
